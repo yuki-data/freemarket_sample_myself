9e7778d3529452198b029065b3c8b6d81583e8ff
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-24 15:46:12 +0900

    add new and create actions to temporary_image_uploader

--- app/controllers/temporary_image_uploader_controller.rb
+++ app/controllers/temporary_image_uploader_controller.rb
@@ -1,4 +1,22 @@
 class TemporaryImageUploaderController < ApplicationController
   def index
+    @temporary_images = TemporaryImage.all
+  end
+
+  def new
+    @temporary_image = TemporaryImage.new
+  end
+
+  def create
+    @temporary_image = TemporaryImage.new(temporary_image_params)
+    if @temporary_image.save
+      redirect_to root_path, notice: "保存しました"
+    else
+      redirect_to root_path, notice: "保存に失敗しました"
+    end
+  end
+
+  def temporary_image_params
+    params.require(:temporary_image).permit(:image)
   end
 end
