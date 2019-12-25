c8d7f919301cef9f3284d439e176cd71df6b9e30
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-25 12:23:30 +0900

    fix typo on basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials.basic_atuth[:username] && Rails.application.credentials.basic_atuth[:password]
+      username == Rails.application.credentials.basic_auth[:username] && Rails.application.credentials.basic_auth[:password]
     end
   end
 end
